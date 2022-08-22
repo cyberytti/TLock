13ab3ec39614936a6b90f5c391cfa308df91cb55
MR.X <107016185+cyberytti@users.noreply.github.com>
2022-08-22 14:01:26 +0530

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,29 @@
+echo "some dependencis are installing........"
+pkg install neofetch -y
+cd 
+cd TLock
+mv lock.py /data/data/com.termux/files/usr/etc
+sleep 1
+echo "file moved"
+sleep 1
+
+cd ../usr/etc
+rm  -rf termux-login.sh
+sleep 1
+echo "file removed"
+sleep 1
+
+cd 
+cd TLock
+cd scripts
+mv termux-login.sh /data/data/com.termux/files/usr/etc
+sleep 1
+echo "final file moved"
+sleep 1
+
+clear
+ 
+echo "Restart your Termux"
+rm -rf TLocik
+cd 
+
